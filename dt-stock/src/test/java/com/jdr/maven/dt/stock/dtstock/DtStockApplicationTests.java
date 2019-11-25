1d042883bb3fe2e9acad621715b2c7f9ae5efff0
JDR <zhoude@vcrecit.com>
2019-11-25 16:29:55 +0800

    dt-stock: 1、modify test class

--- dt-stock/src/test/java/com/jdr/maven/dt/stock/dtstock/DtStockApplicationTests.java
+++ dt-stock/src/test/java/com/jdr/maven/dt/stock/dtstock/DtStockApplicationTests.java
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class DtStockApplicationTests {
+public class DtStockApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
